82dadcb7e1df2760e18a7eb53bb0aa3e9da66a48
kitsunecat <scooping.fox@gmail.com>
2023-04-14 20:09:03 +0900

    feat: add /home, /games pages

---
+++ src/components/games/play.tsx
@@ -0,0 +1,10 @@
+const NewGame = () => {
+  return (
+    <div>
+      <h1>Play Game</h1>
+      <p>This is the new game page.</p>
+    </div>
+  );
+};
+
+export default NewGame;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import GameNew from './components/games/new';
+import GamePlay from './components/games/new';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/games/new" element={<GameNew />} />
+      <Route path="/games/play" element={<GamePlay />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
